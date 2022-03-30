6160d8e1c250522af1ba9345c2b9c4ad04d48f33
MarinaSenoshenko <94277819+MarinaSenoshenko@users.noreply.github.com>
2022-03-30 10:01:21 +0700

    Create ArithmeticExceptions.java

---
+++ src/StackCalc/exceptions/arithmetic/ArithmeticExceptions.java
@@ -0,0 +1,10 @@
+package stackCalc.exceptions.arithmetic;
+
+import stackCalc.exceptions.CalculatorExceptions;
+
+public class ArithmeticExceptions extends CalculatorExceptions {
+
+	public ArithmeticExceptions(String string) {
+		super(string); 
+    }
+}
